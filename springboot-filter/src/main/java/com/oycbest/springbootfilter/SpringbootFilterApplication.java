75e077c130bbcb8284e27a9dd1bb68f7c350c136
ouyangcheng <Oyc20Yqr1314>
2020-04-19 23:03:56 +0800

    spring boot demo filter

---
+++ springboot-filter/src/main/java/com/oycbest/springbootfilter/SpringbootFilterApplication.java
@@ -0,0 +1,15 @@
+package com.oycbest.springbootfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class SpringbootFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFilterApplication.class, args);
+    }
+
+}
