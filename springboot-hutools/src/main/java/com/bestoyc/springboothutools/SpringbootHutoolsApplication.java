b2b421b5fa52d2aec8f8cf75b76535a8788c6a27
oycyqr <1456682842@qq.com>
2020-04-26 15:06:34 +0800

    hu tools

---
+++ springboot-hutools/src/main/java/com/bestoyc/springboothutools/SpringbootHutoolsApplication.java
@@ -0,0 +1,13 @@
+package com.bestoyc.springboothutools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHutoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHutoolsApplication.class, args);
+	}
+
+}
---
+++ springboot-hutools/src/test/java/com/bestoyc/springboothutools/SpringbootHutoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestoyc.springboothutools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHutoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
