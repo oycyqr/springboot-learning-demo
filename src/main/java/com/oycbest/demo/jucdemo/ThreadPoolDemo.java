00d608809606ac61aaaa88be3737e2872a3ccfdf
oycyqr <1456682842@qq.com>
2020-07-15 15:22:56 +0800

    BlockingQueue  demo

--- src/main/java/com/oycbest/demo/jucdemo/ThreadPoolDemo.java
+++ src/main/java/com/oycbest/demo/jucdemo/ThreadPoolDemo.java
@@ -1,7 +1,6 @@
 package com.oycbest.demo.jucdemo;
 
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: oyc
@@ -9,33 +8,45 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Description: 线程池demo
  */
 public class ThreadPoolDemo {
-	public static void main(String[] args) {
-		// 一池五线程
-		ExecutorService threadPool1 = Executors.newFixedThreadPool(5);
-		// 一池一线程
-		ExecutorService threadPool2 = Executors.newSingleThreadExecutor();
-		ExecutorService threadPool3 = Executors.newScheduledThreadPool(5);
-		// 一池N线程
-		ExecutorService threadPool = Executors.newCachedThreadPool();
-		for (int i = 0; i < 6; i++) {
-			//threadPool.execute(()-> System.out.println("demo1:"+Thread.currentThread().getName()));
-		}
-		//ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
-		ExecutorService singleThreadPool = new ThreadPoolExecutor(1, 3,
-				1L, TimeUnit.SECONDS,
-				new LinkedBlockingQueue<Runnable>(3), Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
-		for (int i = 0; i < 10; i++) {
-			//singleThreadPool.execute(()-> System.out.println("demo2:"+Thread.currentThread().getName()));
-		}
-		//singleThreadPool.shutdown();
+    public static void main(String[] args) {
+        // 一池五线程
+        ExecutorService threadPool1 = Executors.newFixedThreadPool(5);
+        // 一池一线程
+        ExecutorService threadPool2 = Executors.newSingleThreadExecutor();
+        ExecutorService threadPool3 = Executors.newScheduledThreadPool(5);
+        // 一池N线程
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 6; i++) {
+            //threadPool.execute(()-> System.out.println("demo1:"+Thread.currentThread().getName()));
+        }
+        //ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
+        ExecutorService singleThreadPool = new ThreadPoolExecutor(1, 3,
+                1L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(3), Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
+        for (int i = 0; i < 10; i++) {
+            //singleThreadPool.execute(()-> System.out.println("demo2:"+Thread.currentThread().getName()));
+        }
+        //singleThreadPool.shutdown();
 
 
-		ExecutorService executor = new ThreadPoolExecutor(
-				1,3,5,TimeUnit.MINUTES,
-				new LinkedBlockingDeque<>(10),Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
-		Future<?> submit = executor.submit(() -> System.out.println(123));
-		System.out.println(submit.isDone());
-		executor.shutdown();
-		System.out.println(submit.isDone());
-	}
+        ExecutorService executor = new ThreadPoolExecutor(
+                2, 5, 5, TimeUnit.MINUTES,
+                new LinkedBlockingDeque<>(10), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        Future<?> submit = executor.submit(() -> System.out.println(123));
+
+        for (int i = 0; i < 20; i++) {
+            executor.submit(() -> {
+                System.out.println("executor demo:" + Thread.currentThread().getName());
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        System.out.println(submit.isDone());
+        executor.shutdown();
+        System.out.println(submit.isDone());
+    }
 }
