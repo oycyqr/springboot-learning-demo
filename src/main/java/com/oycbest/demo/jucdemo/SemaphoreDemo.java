7afac547d37e190d93a0eeb0a22fc5bcb5f0c856
oycyqr <1456682842@qq.com>
2020-07-15 08:53:26 +0800

    semaphore demo

--- src/main/java/com/oycbest/demo/jucdemo/SemaphoreDemo.java
+++ src/main/java/com/oycbest/demo/jucdemo/SemaphoreDemo.java
@@ -10,13 +10,14 @@ import java.util.concurrent.TimeUnit;
 public class SemaphoreDemo {
     public static void main(String[] args) {
         // 5个车位，10辆车
-        Semaphore semaphore = new Semaphore(1, true);
+        Semaphore semaphore = new Semaphore(5, true);
         for (int i = 0; i < 100; i++) {
             new Thread(() -> {
                 try {
                     semaphore.acquire();
-                    System.out.println(Thread.currentThread().getName() + " 车进入");
-                    TimeUnit.MILLISECONDS.sleep(1000);
+                    System.out.println("******* 剩余可用车位：" + (semaphore.availablePermits() + 1) + "  排队车数：" + (semaphore.getQueueLength() + 1));
+                    System.out.println(Thread.currentThread().getName() + " 车进入停车");
+                    TimeUnit.MILLISECONDS.sleep(200);
                     System.out.println(Thread.currentThread().getName() + " 车出场");
                     semaphore.release();
                 } catch (InterruptedException e) {
