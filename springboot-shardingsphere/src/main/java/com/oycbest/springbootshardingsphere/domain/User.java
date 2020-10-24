e793aecc19c3829b8bb617cf4eaf6b2717c57a7b
oycyqr <1456682842@qq.com>
2020-10-24 20:31:08 +0800

    shardingsphere sharing-jdc 数据脱敏

---
+++ springboot-shardingsphere/src/main/java/com/oycbest/springbootshardingsphere/domain/User.java
@@ -0,0 +1,66 @@
+package com.oycbest.springbootshardingsphere.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ * @Description: (User)实体类
+ * @Author oyc
+ * @Date 2020/10/24 16:36
+ * @Version 1.0
+ */
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 358157380505039579L;
+
+    private Integer id;
+
+    /**
+     * 用户名称
+     */
+    private String name;
+
+    /**
+     * 账号
+     */
+    private String account;
+
+    /**
+     * 原始密码
+     */
+    private String password;
+
+    /**
+     * md5加密后的密码
+     */
+    private String md5Password;
+
+    /**
+     * 原始密码
+     */
+    private String password1;
+
+    /**
+     * aes加密后的密码
+     */
+    private String aesPassword;
+
+    /**
+     * 年龄
+     */
+    private String age;
+
+    /**
+     * 性别
+     */
+    private String sex;
+
+    /**
+     * 创建时间
+     */
+    private Date create_time;
+
+}
---
+++ springboot-shardingsphere/src/test/java/com/oycbest/springbootshardingsphere/SpringbootShardingSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oycbest.springbootshardingsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShardingSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
