08c899f80bd4ab1d356f7817099abe177e4af93b
乾源 <1456682842@qq.com>
2020-12-13 10:07:56 +0800

    activiti + SpringbootActivitiApplication

---
+++ springboot-activiti/src/main/java/com/oycbest/activiti/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.oycbest.activiti.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Description: Mybatis-Plus配置类
+ * @Author oyc
+ * @Date 2020/4/29 7:46 下午
+ */
+@EnableTransactionManagement
+@Configuration
+//扫描的mapper文件路径
+@MapperScan("com.oycbest.activiti.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
