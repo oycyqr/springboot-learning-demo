379b3971a6f93830097a56c66962dfbbd0e18f74
乾源 <1456682842@qq.com>
2020-12-25 09:48:21 +0800

    SpringBoot sso

--- springboot-security-oauth2-sso/src/main/java/com/oyc/sso/SpringBootSecurityOAuth2SsoApplication.java
+++ springboot-security-oauth2-sso/src/main/java/com/oyc/sso/SpringBootSecurityOAuth2SsoApplication.java
@@ -3,6 +3,13 @@ package com.oyc.sso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**   
+ * @ClassName: SpringBootSecurityOAuth2SsoApplication  
+ * @Description: SpringBootSecurityOAuth2SsoApplication
+ * @Author oyc 
+ * @Date 2020年12月25日 9:34   
+ * @Version 1.0
+ */
 @SpringBootApplication
 public class SpringBootSecurityOAuth2SsoApplication {
 
