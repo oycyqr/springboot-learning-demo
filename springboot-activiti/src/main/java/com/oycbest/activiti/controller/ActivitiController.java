6f76ca11cb90693d2584462038a78d8e9d0d01ac
乾源 <1456682842@qq.com>
2021-01-07 12:04:58 +0800

    springboot 整合Activiti

---
+++ springboot-activiti/src/main/java/com/oycbest/activiti/controller/ActivitiController.java
@@ -0,0 +1,345 @@
+package com.oycbest.activiti.controller;
+
+import com.oycbest.activiti.service.impl.ActivityServiceImpl;
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.history.*;
+import org.activiti.engine.identity.User;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntityImpl;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.task.Task;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @ClassName ActivitiController
+ * @Description TODO
+ * @Author oyc
+ * @Date 2020/12/13 23:19
+ * @Version
+ */
+@RestController
+@RequestMapping("act")
+public class ActivitiController {
+
+    @Autowired
+    private IdentityService identityService;
+
+    @RequestMapping("list")
+    public String getProcessList() {
+        User ouyang = identityService.newUser("ouyang");
+        identityService.saveUser(ouyang);
+        //List<String> userInfoKeys = identityService.getUserInfoKeys("ouyang");
+        return "getProcessList:";
+    }
+
+    /**
+     * 1.启动流程
+     * 2.根据用户名获取待办流程 列表  （传入用户名）
+     * 3.根据待办task id完成流程    （传入任务id）
+     */
+    public void startProccessInstance() {
+        ProcessDefinition processDefinition = new ProcessDefinitionEntityImpl();
+    }
+
+    @Resource
+    private RepositoryService repositoryService;
+    @Resource
+    private ActivityServiceImpl activityService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private HistoryService historyService;
+
+    /**
+     * 部署流程
+     */
+    @RequestMapping("initProcesses")
+    public void initProcesses() {
+        //部署对象
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("processes/holiday.bpmn")// bpmn文件
+                .addClasspathResource("processes/holiday.png")// 图片文件
+                .name("请假申请流程")
+                .deploy();
+        System.out.println("流程部署id:" + deployment.getId());
+        System.out.println("流程部署名称:" + deployment.getName());
+    }
+
+    //启动流程---215001
+    @RequestMapping("startPro")
+    public void startPro(@Param("processesId") String processesId) {
+        activityService.startProcesses(processesId);
+    }
+
+    //获取受理员任务列表
+    @RequestMapping("findTasksListByUserName")
+    public void findTasksListByUserName(@Param("userName") String userName) {
+        List<Task> lists = activityService.findTasksByUserId(userName);
+        System.out.println("任务列表：" + lists);//任务列表：[Task[id=5005, name=填写请假申请单]]
+    }
+
+    //受理员受理数据
+    @RequestMapping("completeTasksForSL")
+    public void completeTasksForSL() {
+        activityService.completeTask("210028", "sly1", "true");//受理后，任务列表数据减少
+    }
+
+    //获取审批员任务列表
+    @RequestMapping("findTasksForSP")
+    public void findTasksForSP() {
+        List<Task> lists = activityService.findTasksByUserId("spy1");
+        System.out.println("任务列表：" + lists);//任务列表：[Task[id=220004, name=审批]]
+    }
+
+    //审批员通过审核
+    @RequestMapping("completeTask")
+    public void completeTasksForSP() {
+        activityService.completeTask("220004", "spy1", "true");//审批后，任务列表数据减少
+    }
+
+
+    //设置流程变量
+    //设置流程变量【基本类型】
+    @RequestMapping("setTasksVar")
+    public void setTasksVar() {
+        List<Task> lists = activityService.findTasksByUserId("sly1");
+        for (Task task : lists) {//不知为何，变量保存成功，但数据表只有请假天数含有任务id，单获取流程变量时，根据任务id均可获取到（如下一测试）
+            taskService.setVariable(task.getId(), "请假人", "sly1");
+            taskService.setVariableLocal(task.getId(), "请假天数", 3);
+            taskService.setVariable(task.getId(), "请假日期", new Date());
+        }
+    }
+
+    //获取流程变量
+    @RequestMapping("getTasksVar")
+    public void getTasksVar() {
+        List<Task> lists = activityService.findTasksByUserId("sly1");
+        for (Task task : lists) {
+            //获取流程变量【基本类型】
+            String person = (String) taskService.getVariable(task.getId(), "请假人");
+            Integer day = (Integer) taskService.getVariableLocal(task.getId(), "请假天数");
+            Date date = (Date) taskService.getVariable(task.getId(), "请假日期");
+
+            System.out.println("流程变量：" + person + "||" + day + "||" + date + "||");
+        }
+    }
+
+    //设置流程变量【实体】
+    @RequestMapping("setTasksVarEntity")
+    public void setTasksVarEntity() {
+        List<Task> lists = activityService.findTasksByUserId("sly1");
+        for (Task task : lists) {
+           /* Person p = new Person();
+            p.setName("翠花");
+            p.setId(20);
+            p.setDate();;
+            p.setNote("回去探亲，一起吃个饭123");*/
+            taskService.setVariable(task.getId(), "人员信息(添加固定版本)", "oy");
+
+            System.out.println("设置流程变量成功！");
+        }
+    }
+
+    //获取流程变量【实体】  实体必须序列化
+    @RequestMapping("getTasksVarEntity")
+    public void getTasksVarEntity() {
+        List<Task> lists = activityService.findTasksByUserId("sly1");
+        for (Task task : lists) {
+            // 2.获取流程变量，使用javaBean类型
+            Object variable = taskService.getVariable(task.getId(), "人员信息(添加固定版本)");
+            System.out.println(variable);
+            //System.out.println(" 请假人：  "+p.getName()+"  请假天数：  "+p.getId()+"   请假时间："+ p.getDate()+ "   请假原因： "+p.getNote());
+        }
+    }
+
+
+    //生成流程图---232501
+    @RequestMapping("queryProImg")
+    public void queryProImg() throws Exception {
+        activityService.queryProImg("232501");
+    }
+
+    //生成流程图（高亮）---232501
+    @RequestMapping("queryProHighLighted")
+    public void queryProHighLighted() throws Exception {
+        activityService.queryProHighLighted("232501");
+    }
+
+    /**
+     * 查询流程变量的历史表,可以根据变量名称查询该变量的所有历史信息
+     */
+    @RequestMapping("findHistoryProcessVariables")
+    public void findHistoryProcessVariables() {
+        List<HistoricVariableInstance> list = historyService.createHistoricVariableInstanceQuery()//创建一个历史的流程变量查询对象
+                .variableName("请假天数")
+                .list();
+        if (list != null && list.size() > 0) {
+            for (HistoricVariableInstance hvi : list) {
+                System.out.println(hvi.getId() + "     " + hvi.getProcessInstanceId() + "   " + hvi.getVariableName()
+                        + "   " + hvi.getVariableTypeName() + "    " + hvi.getValue());
+                System.out.println("########################################");
+            }
+        }
+
+    }
+
+
+    /**
+     * 历史流程实例查询
+     * http://blog.csdn.net/luckyzhoustar/article/details/48652783
+     */
+    @RequestMapping("findHistoricProcessInstance")
+    public void findHistoricProcessInstance() {
+        // 查询已完成的流程
+        List<HistoricProcessInstance> datas = historyService
+                .createHistoricProcessInstanceQuery().finished().list();
+        System.out.println("使用finished方法：" + datas.size());
+        // 根据流程定义ID查询
+        datas = historyService.createHistoricProcessInstanceQuery()
+                .processDefinitionId("processDefinitionId").list();
+        System.out.println("使用processDefinitionId方法： " + datas.size());
+        // 根据流程定义key（流程描述文件的process节点id属性）查询
+        datas = historyService.createHistoricProcessInstanceQuery()
+                .processDefinitionKey("processDefinitionKey").list();
+        System.out.println("使用processDefinitionKey方法： " + datas.size());
+        // 根据业务主键查询
+        datas = historyService.createHistoricProcessInstanceQuery()
+                .processInstanceBusinessKey("processInstanceBusinessKey").list();
+        System.out.println("使用processInstanceBusinessKey方法： " + datas.size());
+        // 根据流程实例ID查询
+        datas = historyService.createHistoricProcessInstanceQuery()
+                .processInstanceId("processInstanceId").list();
+        System.out.println("使用processInstanceId方法： " + datas.size());
+        // 查询没有完成的流程实例
+        historyService.createHistoricProcessInstanceQuery().unfinished().list();
+        System.out.println("使用unfinished方法： " + datas.size());
+    }
+
+    /**
+     * 历史任务查询
+     *
+     * @throws ParseException
+     */
+    @RequestMapping("findHistoricTasks")
+    public void findHistoricTasks() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        //历史数据查询
+        List<HistoricTaskInstance> datas = historyService.createHistoricTaskInstanceQuery()
+                .finished().list();
+        System.out.println("使用finished方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .processDefinitionId("processDefinitionId").list();
+        System.out.println("使用processDefinitionId方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .processDefinitionKey("testProcess").list();
+        System.out.println("使用processDefinitionKey方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .processDefinitionName("testProcess2").list();
+        System.out.println("使用processDefinitionName方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .processFinished().list();
+        System.out.println("使用processFinished方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .processInstanceId("processInstanceId").list();
+        System.out.println("使用processInstanceId方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .processUnfinished().list();
+        System.out.println("使用processUnfinished方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .taskAssignee("crazyit").list();
+        System.out.println("使用taskAssignee方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .taskAssigneeLike("%zy%").list();
+        System.out.println("使用taskAssigneeLike方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .taskDefinitionKey("usertask1").list();
+        System.out.println("使用taskDefinitionKey方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .taskDueAfter(sdf.parse("2020-10-11 06:00:00")).list();
+        System.out.println("使用taskDueAfter方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .taskDueBefore(sdf.parse("2022-10-11 06:00:00")).list();
+        System.out.println("使用taskDueBefore方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .taskDueDate(sdf.parse("2020-10-11 06:00:00")).list();
+        System.out.println("使用taskDueDate方法查询：" + datas.size());
+        datas = historyService.createHistoricTaskInstanceQuery()
+                .unfinished().list();
+        System.out.println("使用unfinished方法查询：" + datas.size());
+    }
+
+    /**
+     * 历史行为查询
+     * 流程在进行过程中，每每走一个节点，都会记录流程节点的信息，包括节点的id，名称、类型、时间等，保存到ACT_HI_ACTINST表中。
+     *
+     * @throws ParseException
+     */
+    @RequestMapping("findHistoricActivityInstance")
+    public void findHistoricActivityInstance() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        //查询数据
+        List<HistoricActivityInstance> datas = historyService.createHistoricActivityInstanceQuery()
+                .activityId("endevent1").list();
+        System.out.println("使用activityId查询：" + datas.size());
+        datas = historyService.createHistoricActivityInstanceQuery()
+                .activityInstanceId(datas.get(0).getId()).list();
+        System.out.println("使用activityInstanceId查询：" + datas.size());
+        datas = historyService.createHistoricActivityInstanceQuery()
+                .activityType("intermediateSignalCatch").list();
+        System.out.println("使用activityType查询：" + datas.size());
+        datas = historyService.createHistoricActivityInstanceQuery()
+                .executionId("executionId").list();
+        System.out.println("使用executionId查询：" + datas.size());
+        datas = historyService.createHistoricActivityInstanceQuery().finished().list();
+        System.out.println("使用finished查询：" + datas.size());
+        datas = historyService.createHistoricActivityInstanceQuery()
+                .processInstanceId("processInstanceId").list();
+        System.out.println("使用processInstanceId查询：" + datas.size());
+        datas = historyService.createHistoricActivityInstanceQuery()
+                .taskAssignee("crazyit").list();
+        System.out.println("使用taskAssignee查询：" + datas.size());
+        datas = historyService.createHistoricActivityInstanceQuery().unfinished().list();
+        System.out.println("使用unfinished查询：" + datas.size());
+    }
+
+    /**
+     * 历史流程明细查询
+     * 在流程进行的过程中，会产生许多明细数据，只有将History设置为最高级别的时候，才会被记录到ACT_HI_DETAIL表中。
+     *
+     * @throws ParseException
+     */
+    @RequestMapping("findHistoricDetail")
+    public void findHistoricDetail() {
+        // 查询历史行为
+        HistoricActivityInstance act = historyService.createHistoricActivityInstanceQuery()
+                .activityName("First Task").finished().singleResult();
+        List<HistoricDetail> datas = historyService.createHistoricDetailQuery()
+                .activityInstanceId(act.getId()).list();
+        System.out.println("使用activityInstanceId方法查询：" + datas.size());
+        datas = historyService.createHistoricDetailQuery().excludeTaskDetails().list();
+        System.out.println("使用excludeTaskDetails方法查询：" + datas.size());
+        datas = historyService.createHistoricDetailQuery().formProperties().list();
+        System.out.println("使用formProperties方法查询：" + datas.size());
+        datas = historyService.createHistoricDetailQuery().processInstanceId("processInstanceId").list();
+        System.out.println("使用processInstanceId方法查询：" + datas.size());
+        datas = historyService.createHistoricDetailQuery().taskId("taskId").list();
+        System.out.println("使用taskId方法查询：" + datas.size());
+        datas = historyService.createHistoricDetailQuery().variableUpdates().list();
+        System.out.println("使用variableUpdates方法查询：" + datas.size());
+    }
+
+}
---
+++ springboot-activiti/src/main/java/com/oycbest/activiti/service/impl/ActivityServiceImpl.java
@@ -0,0 +1,232 @@
+package com.oycbest.activiti.service.impl;
+
+
+import org.activiti.bpmn.model.BpmnModel;
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.history.HistoricActivityInstance;
+import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.image.ProcessDiagramGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.*;
+
+/**
+ * @ClassName: ActivityServiceImpl
+ * @Description: ActivityServiceImpl
+ * @Author oyc
+ * @Date 2021/1/7 10:35
+ * @Version 1.0
+ */
+@Service
+public class ActivityServiceImpl {
+
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private HistoryService historyService;
+    @Autowired
+    private RepositoryService repositoryService;
+    @Autowired
+    private ProcessEngineConfigurationImpl processEngineConfiguration;
+
+    /**
+     * 启动流程
+     *
+     * @param bizId 业务id
+     */
+    public void startProcesses(String bizId) {
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey("holiday", bizId);//流程图id，业务表id
+        System.out.println("流程启动成功，流程id:" + pi.getId());
+    }
+
+    /**
+     * <p>描述: 根据用户id查询待办任务列表</p>
+     *
+     * @author 范相如
+     * @date 2018年2月25日
+     */
+    public List<Task> findTasksByUserId(String userId) {
+        List<Task> resultTask = taskService.createTaskQuery().processDefinitionKey("holiday").taskCandidateOrAssigned(userId).list();
+        return resultTask;
+    }
+
+    /**
+     * <p>描述:任务审批 	（通过/拒接） </p>
+     *
+     * @param taskId 任务id
+     * @param userId 用户id
+     * @param result false OR true
+     * @author 范相如
+     * @date 2018年2月25日
+     */
+    public void completeTask(String taskId, String userId, String result) {
+        //获取流程实例
+        taskService.claim(taskId, userId);
+
+        Map<String, Object> vars = new HashMap<String, Object>();
+        vars.put("sign", "true");
+        taskService.complete(taskId, vars);
+    }
+
+    /**
+     * 更改业务流程状态#{ActivityDemoServiceImpl.updateBizStatus(execution,"tj")}
+     *
+     * @param execution
+     * @param status
+     */
+    public void updateBizStatus(DelegateExecution execution, String status) {
+        String bizId =  execution.getProcessInstanceBusinessKey();
+        //根据业务id自行处理业务表
+        System.out.println("业务表[" + bizId + "]状态更改成功，状态更改为：" + status);
+    }
+
+
+    //流程节点权限用户列表${ActivityDemoServiceImpl.findUsers(execution,sign)}
+    public List<String> findUsersForSL(DelegateExecution execution) {
+        return Arrays.asList("sly1", "sly2");
+    }
+
+    //流程节点权限用户列表${ActivityDemoServiceImpl.findUsers(execution,sign)}
+    public List<String> findUsersForSP(DelegateExecution execution) {
+        return Arrays.asList("spy1", "uspy2");
+    }
+
+    /**
+     * <p>描述:  生成流程图
+     * 首先启动流程，获取processInstanceId，替换即可生成</p>
+     *
+     * @param processInstanceId
+     * @throws Exception
+     * @author 范相如
+     * @date 2018年2月25日
+     */
+    public void queryProImg(String processInstanceId) throws Exception {
+        //获取历史流程实例
+        HistoricProcessInstance processInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+
+        //根据流程定义获取输入流
+        InputStream is = repositoryService.getProcessDiagram(processInstance.getProcessDefinitionId());
+        BufferedImage bi = ImageIO.read(is);
+        File file = new File("demo2.png");
+        if (!file.exists()) file.createNewFile();
+        FileOutputStream fos = new FileOutputStream(file);
+        ImageIO.write(bi, "png", fos);
+        fos.close();
+        is.close();
+        System.out.println("图片生成成功");
+
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateUser("userId").list();
+        for (Task t : tasks) {
+            System.out.println(t.getName());
+        }
+    }
+
+
+    /**
+     * 流程图高亮显示
+     * 首先启动流程，获取processInstanceId，替换即可生成
+     *
+     * @throws Exception
+     */
+    public void queryProHighLighted(String processInstanceId) throws Exception {
+        //获取历史流程实例
+        HistoricProcessInstance processInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+        //获取流程图
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(processInstance.getProcessDefinitionId());
+
+        ProcessDiagramGenerator diagramGenerator = processEngineConfiguration.getProcessDiagramGenerator();
+        ProcessDefinitionEntity definitionEntity = (ProcessDefinitionEntity) repositoryService.getProcessDefinition(processInstance.getProcessDefinitionId());
+
+        List<HistoricActivityInstance> highLightedActivitList = historyService.createHistoricActivityInstanceQuery().processInstanceId(processInstanceId).list();
+        //高亮环节id集合
+        List<String> highLightedActivitis = new ArrayList<String>();
+
+        //高亮线路id集合
+        List<String> highLightedFlows = getHighLightedFlows(definitionEntity, highLightedActivitList);
+
+        for (HistoricActivityInstance tempActivity : highLightedActivitList) {
+            String activityId = tempActivity.getActivityId();
+            highLightedActivitis.add(activityId);
+        }
+        //配置字体
+        InputStream imageStream = diagramGenerator.generateDiagram(bpmnModel, "png", highLightedActivitis, highLightedFlows, "宋体", "微软雅黑", "黑体", null, 2.0);
+        BufferedImage bi = ImageIO.read(imageStream);
+        File file = new File("demo2.png");
+        if (!file.exists()) file.createNewFile();
+        FileOutputStream fos = new FileOutputStream(file);
+        ImageIO.write(bi, "png", fos);
+        fos.close();
+        imageStream.close();
+        System.out.println("图片生成成功");
+    }
+
+    /**
+     * 获取需要高亮的线
+     *
+     * @param processDefinitionEntity
+     * @param historicActivityInstances
+     * @return
+     */
+    private List<String> getHighLightedFlows(
+            ProcessDefinitionEntity processDefinitionEntity,
+            List<HistoricActivityInstance> historicActivityInstances) {
+
+        List<String> highFlows = new ArrayList<String>();// 用以保存高亮的线flowId
+        for (int i = 0; i < historicActivityInstances.size() - 1; i++) {// 对历史流程节点进行遍历
+            /*processDefinitionEntity.
+            ActivityImpl activityImpl = processDefinitionEntity
+                    .findActivity(historicActivityInstances.get(i)
+                            .getActivityId());// 得到节点定义的详细信息
+            List<ActivityImpl> sameStartTimeNodes = new ArrayList<ActivityImpl>();// 用以保存后需开始时间相同的节点
+            ActivityImpl sameActivityImpl1 = processDefinitionEntity
+                    .findActivity(historicActivityInstances.get(i + 1)
+                            .getActivityId());
+            // 将后面第一个节点放在时间相同节点的集合里
+            sameStartTimeNodes.add(sameActivityImpl1);
+            for (int j = i + 1; j < historicActivityInstances.size() - 1; j++) {
+                HistoricActivityInstance activityImpl1 = historicActivityInstances
+                        .get(j);// 后续第一个节点
+                HistoricActivityInstance activityImpl2 = historicActivityInstances
+                        .get(j + 1);// 后续第二个节点
+                if (activityImpl1.getStartTime().equals(
+                        activityImpl2.getStartTime())) {
+                    // 如果第一个节点和第二个节点开始时间相同保存
+                    ActivityImpl sameActivityImpl2 = processDefinitionEntity
+                            .findActivity(activityImpl2.getActivityId());
+                    sameStartTimeNodes.add(sameActivityImpl2);
+                } else {
+                    // 有不相同跳出循环
+                    break;
+                }
+            }
+            List<PvmTransition> pvmTransitions = activityImpl
+                    .getOutgoingTransitions();// 取出节点的所有出去的线
+            for (PvmTransition pvmTransition : pvmTransitions) {
+                // 对所有的线进行遍历
+                ActivityImpl pvmActivityImpl = (ActivityImpl) pvmTransition
+                        .getDestination();
+                // 如果取出的线的目标节点存在时间相同的节点里，保存该线的id，进行高亮显示
+                if (sameStartTimeNodes.contains(pvmActivityImpl)) {
+                    highFlows.add(pvmTransition.getId());
+                }
+            }*/
+        }
+        return highFlows;
+    }
+}
